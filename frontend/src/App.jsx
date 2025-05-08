0c6def7b8c9fef5d8e06e152f57224eaa83d116d
Comagol <comabellafrancisco@gmail.com>
2025-05-08 19:40:08 -0300

    agregada carpeta backend como parte del repo principal

---
+++ frontend/src/App.jsx
@@ -0,0 +1,11 @@
+import { Box, Heading } from '@chakra-ui/react';
+
+function App() {
+  return (
+    <Box p={8}>
+      <Heading>Bienvenido a The Ad Trend</Heading>
+    </Box>
+  );
+}
+
+export default App;
