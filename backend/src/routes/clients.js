ce9a6168e223eb18d817166192d73b2a175ebc1c
Comagol <comabellafrancisco@gmail.com>
2025-05-26 00:03:09 -0300

    fixxing problem to run npm run dev

--- backend/src/routes/clients.js
+++ backend/src/routes/clients.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const router = express.Router();
 const clientController = require('../controllers/clientController');
-const auth = require('../middleware/auth');
+const { auth } = require('../middleware/auth');
 
 // Todas las rutas requieren autenticación
 router.use(auth);
